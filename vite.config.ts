82ba2dbfc116f05499618181d60c8d89a01841e1
Kacper Kruczek <>
2022-02-03 16:16:50 +0100

    junit reporter

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    reporters: "junit",
+    outputFile: "junit.xml",
   },
 });
