ed36b5d36cbb1e7b4e3ac90551c7ea29a4f511b7
Kacper Kruczek <>
2022-02-10 10:33:09 +0100

    added unocss while checking this package

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function App({ initialCounter = 0 }) {
   const [counter, setCounter] = useState(initialCounter);
-
+  const classNames =
+    "w-64 m-auto border border-emerald-500 border-2 hover:bg-emerald-100 p-2 flex flex-col mt-3 gap-5";
   return (
-    <div>
-      counter:{counter}
+    <div className={classNames}>
+      <div>counter: {counter}</div>
       <button
         onClick={() => {
           setCounter((prevCounter) => prevCounter + 1);
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,42 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
+import Unocss from "unocss/vite";
 import { defineConfig } from "vite";
-
+import { presetUno, presetAttributify } from "unocss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unocss({
+      theme: {
+        colors: {
+          emerald: {
+            "50": "#edf4f2",
+            "100": "#ccefed",
+            "200": "#93e8d7",
+            "300": "#58cfad",
+            "400": "#1eb27e",
+            "500": "#159955",
+            "600": "#138440",
+            "700": "#136635",
+            "800": "#0f4729",
+            "900": "#0b2c21",
+          },
+        },
+        breakpoints: {
+          xs: "320px",
+          sm: "640px",
+        },
+      },
+      presets: [
+        presetAttributify({
+          /* preset options */
+        }),
+        presetUno({}),
+      ],
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
